b75bc4644a3c7fee9729c2e5769369a353bc0c5f
ArifDev-coder <achadzainul67@gmail.com>
2024-08-23 22:00:36 +0700

    ok

---
+++ A-Object/main.js
@@ -0,0 +1,12 @@
+// Cara untuk membuat object pada javacript
+// 1.Object Literal
+// 2.Function Declaration
+// 3.Constructor Function
+// 4.Object.create();
+
+let mahaSiswa = {
+    nama: 'Arif',
+    energi: 10,
+}
+
+console.log(mahaSiswa)
