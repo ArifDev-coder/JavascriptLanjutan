03a9d9862d8c0e55c86013eb752a38337e7b11c0
Arif DesktopMode <achadzainul67@gmail,con>
2024-08-26 16:51:19 +0700

    update

---
+++ D-ArrowFunction/main.js
@@ -0,0 +1,26 @@
+// Funcion Expression
+// const tampilNama = function (nama) {
+//     return nama;
+// }
+// console.log(tampilNama('Arif'));
+
+
+// Arrow Funtion
+// const tampilNama = nama => {return `Halo, ${nama}`};
+// console.log('Arif');
+
+// implisit return
+// const tampilNama = (nama, waktu) => `Halo ${nama}, selamat ${waktu}`;
+// console.log(tampilNama('Arif', 'Pagi'));
+
+// const tampilNama = () => `Hello world!`;
+// console.log(tampilNama());
+
+let num = ['satu', 'dua', 'tiga', 'empat'];
+// let jumlahHuruf = num.map(nama => nama.length);
+// console.log(jumlahHuruf);
+let jumlahHuruf = num.map(number => ({
+    number,
+    jumlahHuruf: number.length
+}));
+console.table(jumlahHuruf)
