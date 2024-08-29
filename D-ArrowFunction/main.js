de05fa9c1420d18cf0c8abd9e4c80e516a1d2c21
Arif DesktopMode <achadzainul67@gmail,con>
2024-08-29 15:17:00 +0700

    a

--- D-ArrowFunction/main.js
+++ D-ArrowFunction/main.js
@@ -16,11 +16,47 @@
 // const tampilNama = () => `Hello world!`;
 // console.log(tampilNama());
 
-let num = ['satu', 'dua', 'tiga', 'empat'];
+// let num = ['satu', 'dua', 'tiga', 'empat'];
 // let jumlahHuruf = num.map(nama => nama.length);
 // console.log(jumlahHuruf);
-let jumlahHuruf = num.map(number => ({
-    number,
-    jumlahHuruf: number.length
-}));
-console.table(jumlahHuruf);
+// let jumlahHuruf = num.map(number => ({
+//     number,
+//     jumlahHuruf: number.length
+// }));
+// console.table(jumlahHuruf);
+
+
+// KONSEP THIS PADA ARROW FUNCTION
+
+// const Siswa = function () {
+//     this.nama = 'Arif';
+//     this.umur = 14;
+//     this.sayHello = function () {
+//         console.log(`Halo,  nama saya ${this.nama}, umur saya ${this.umur}`);
+//     }
+// }
+
+// const arif = new Siswa();
+
+// Arrow function
+// const Siswa = function () {
+//     this.nama = 'Arif';
+//     this.umur = 14;
+//     this.sayHello = () => {
+//         console.log(`Halo,  nama saya ${this.nama}, umur saya ${this.umur}`);
+//     }
+// }
+
+// const arif = new Siswa();
+// arif.sayHello();
+
+// Object Literal
+// const siswa1 = {
+//     nama: 'Arif',
+//     umur: 14,
+//     sayHello: () => {
+//         console.log(`Halo,  nama saya ${this.nama}, umur saya ${this.umur}`);
+//         console.log(this);
+//     }
+// }
+// siswa1.sayHello();
