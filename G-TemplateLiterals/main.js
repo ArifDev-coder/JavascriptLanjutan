5806882df587cbccad6b4090960aa19574ee09fe
Arif DesktopMode <achadzainul67@gmail,con>
2024-09-08 22:25:12 +0700

    mantap

---
+++ G-TemplateLiterals/main.js
@@ -0,0 +1,97 @@
+// Template Literals / Template String
+
+// const nama = 'Arif';
+// const umur = 14;
+
+// console.log('Halo nama saya ' + nama + ', dan saya' + umur + ' tahun.')
+// console.log(`Halo nama saya ${nama}, dan saya ${umur} tahun.`);
+
+
+// Embedded Expressions
+// sayHello = () => `Halo nama saya ${nama}, dan saya ${umur} tahun.`
+// const x = 10
+
+// console.log(`${1 + 1}`);
+// console.log(`${sayHello()}`);
+// console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`); // -> operator ternary
+
+// HTML Fragments
+// const Siswa = {
+//     nama: 'Achmad Zainul Arif',
+//     umur: 14,
+//     phone: 32343434,
+//     email: 'anjayjs12@gmail.com'
+// };
+// const el = `
+// <div class="siswa">
+//     <h2>Nama Siswa : ${Siswa.nama}</h2>
+//     <span class="phone">Nomor Hp Siswa: ${Siswa.phone}</span>
+// </div>
+// `
+
+// 2. looping
+// const sws = [
+//     {
+//         nama: 'Arif',
+//         umur: 12,
+//     },
+//     {
+//         nama: 'Zainul',
+//         umur: 14,
+//     },
+//     {
+//         nama: 'Achmad',
+//         umur: 45,
+//     }
+// ]
+// const el = `
+// <div class="siswa">
+//     <h2>Nama Siswa : ${sws.nama}</h2>
+//     ${sws.map(m => `<ul>
+//             <li>Nama: ${m.nama}</li>
+//             <li>Umur: ${m.umur}</li>
+//         </ul>`).join('')}
+// </div>
+// `
+
+// 3. Conditionals
+// ternary
+// const lagu = {
+//     judul: 'Death Bed',
+//     penyanyi: 'Powfu, beabadoobee',
+//     feat: 'coffee for your head'
+// }
+// const el = `<div class="lagu">
+//     <ul>
+//         <li>${lagu.judul}</li>
+//         <li>${lagu.penyanyi} ${lagu.feat ? `(feat. ${lagu.feat})` : ''}</li>
+//     </ul>
+// </div>`
+
+// 4. Nested
+// HTML Fragments bersarang
+const ssw = {
+    nama: 'Arif',
+    kelas: 8,
+    Ekstrakurikuler: [
+        `Taekwondo`,
+        `Pemprograman`
+    ]
+}
+
+function cetakEkstrakurikuler(Ekstrakurikuler) {
+    return `
+        <ol>
+            ${Ekstrakurikuler.map(mk => `<li>${mk}</li>`).join('')}
+        </ol>
+    `
+}
+
+const el = `<div class="ssw">
+    <h2>Nama: ${ssw.nama}</h2>
+    <span class="Kelas">Kelas: ${ssw.kelas}</span>
+    <h4>Ekstrakurikuler: </h4>
+    ${cetakEkstrakurikuler(ssw.Ekstrakurikuler)}
+</div>`
+
+document.body.innerHTML = el;
